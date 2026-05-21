ffdffb82821fbae7a5b2f42d73b46ab47f4d75a0
Mateus Ramos <mateusramosv@hotmail.com>
2026-05-21 18:30:18 -0300

    Criando teste. Teste de contagem de produtos

---
+++ src/test/java/iterator/InventarioTest.java
@@ -0,0 +1,23 @@
+package iterator;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class InventarioTest {
+
+    @Test
+    void deveContarProdutosVencidosNoEstoque() {
+        Estoque estoque = new Estoque(
+                new Produto("Leite Integral", true),
+                new Produto("Arroz 5kg", false),
+                new Produto("Iogurte Morango", true),
+                new Produto("Feijão Preto", false)
+        );
+
+
+        assertEquals(2, Inventario.contarProdutosVencidos(estoque));
+    }
+
+
+
+}

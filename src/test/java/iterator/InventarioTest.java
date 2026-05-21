ba6795427aabc76fe90f510a7cc03cadc18fbf95
Mateus Ramos <mateusramosv@hotmail.com>
2026-05-21 18:31:47 -0300

    incluindo teste de contagem dos produtos

--- src/test/java/iterator/InventarioTest.java
+++ src/test/java/iterator/InventarioTest.java
@@ -18,6 +18,21 @@ class InventarioTest {
         assertEquals(2, Inventario.contarProdutosVencidos(estoque));
     }
 
+    @Test
+    void deveContarTotalDeProdutosNoEstoque() {
+        Estoque estoque = new Estoque(
+                new Produto("Leite Integral", true),
+                new Produto("Arroz 5kg", false),
+                new Produto("Iogurte Morango", true),
+                new Produto("Feijão Preto", false)
+        );
+
+
+        assertEquals(4, Inventario.contarTotalProdutos(estoque));
+    }
+
+
+
 
 
 }
